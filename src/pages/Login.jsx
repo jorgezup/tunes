cca5ec61e212d85ebd174ce6fd14eb87c0c411d2
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-02-03 16:03:14 -0400

    feat: requirement 02

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,58 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
+import Loading from '../components/Loading';
+
+import { createUser } from '../services/userAPI';
+
+const MIN_LENGHT = 3;
 export default class Login extends Component {
+  state = {
+    name: '',
+    isDisabled: true,
+    message: '',
+    isClicked: false,
+  }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({
+      name: value,
+      isDisabled: !this.isCorrectLogin(value),
+    });
+  }
+
+  handleSubmit = async () => {
+    this.setState({ isClicked: true });
+    const { name } = this.state;
+    const response = await createUser({ name });
+    this.setState({ message: response });
+  }
+
+  isCorrectLogin = (str) => str.length >= MIN_LENGHT;
+
   render() {
-    return <div data-testid="page-login">Login</div>;
+    const { isDisabled, isClicked, message } = this.state;
+    return (
+      <div data-testid="page-login">
+        Login
+        <form action="">
+          <input
+            type="text"
+            data-testid="login-name-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            onClick={ this.handleSubmit }
+            disabled={ isDisabled }
+          >
+            Entrar
+          </button>
+          {isClicked && <Loading /> }
+          {message && <Redirect to="/search" />}
+        </form>
+      </div>
+    );
   }
 }
