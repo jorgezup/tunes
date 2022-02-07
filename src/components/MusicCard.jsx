cf834c1c064759c2d7a719002e82405faa951b98
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-02-07 17:23:51 -0400

    feat: added input type checknox to favorite song

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -3,7 +3,14 @@ import PropTypes from 'prop-types';
 
 export default class MusicCard extends Component {
   render() {
-    const { music: { previewUrl, trackName } } = this.props;
+    const {
+      music: {
+        previewUrl,
+        trackName,
+        trackId,
+      },
+      handleFavorite,
+    } = this.props;
     return (
       <div>
         <p>{trackName}</p>
@@ -14,11 +21,22 @@ export default class MusicCard extends Component {
           <code>audio</code>
           .
         </audio>
+        <label htmlFor="favorite">
+          Favorita
+          <input
+            type="checkbox"
+            name="favorite"
+            onChange={ handleFavorite }
+            data-testid={ `checkbox-music-${trackId}` }
+          />
+        </label>
       </div>
+
     );
   }
 }
 
 MusicCard.propTypes = {
   music: PropTypes.element.isRequired,
+  handleFavorite: PropTypes.func.isRequired,
 };
